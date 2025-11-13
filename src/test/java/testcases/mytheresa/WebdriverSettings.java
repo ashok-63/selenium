0b8ca97fa873d98ac0f3aa7486cb77ba148e9adb
ashok-63 <157099924+ashok-63@users.noreply.github.com>
2025-11-13 16:14:39 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -1,5 +1,25 @@
-System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+package testcases.mytheresa;
 
-ChromeOptions options = new ChromeOptions();
-options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-driver = new ChromeDriver(options);
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebdriverSettings {
+
+    protected static WebDriver driver;
+
+    public static void initialize() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Important for Jenkins/Docker
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
