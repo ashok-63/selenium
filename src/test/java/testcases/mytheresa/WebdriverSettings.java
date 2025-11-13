a846ac067f0e47142aa8828d23f177f3c0e1ed9e
ashok-63 <157099924+ashok-63@users.noreply.github.com>
2025-11-13 14:19:11 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -1,23 +1,5 @@
-package testcases.mytheresa;
+System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-public class WebdriverSettings {
-    public static WebDriver driver;
-
-    public static void driverSettings() {
-        // Use system-wide ChromeDriver (installed in Jenkins Docker container)
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Run in headless mode (no GUI)
-        options.addArguments("--no-sandbox"); // Required in Docker
-        options.addArguments("--disable-dev-shm-usage"); // Prevents crash on shared memory
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-
-        driver = new ChromeDriver(options);
-    }
-}
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+driver = new ChromeDriver(options);
