af0a23d5894261f4613f681448078dc88e742b99
AshokS <ashok.shinde63@gmail.com>
2025-11-14 15:55:51 +0530

    Fixed test classes and WebDriver setup for Jenkins

--- src/test/java/testcases/mytheresa/NewUserJourney.java
+++ src/test/java/testcases/mytheresa/NewUserJourney.java
@@ -1,73 +1,17 @@
 package testcases.mytheresa;
 
-<<<<<<< HEAD
-import base.BaseTest;
+import base.WebdriverSettings;
 import org.testng.annotations.Test;
 
-public class NewUserJourney extends BaseTest {
+public class NewUserJourney extends WebdriverSettings {
 
     @Test
     public void testUserJourney() {
 
-        driver.get("https://www.mytheresa.com/");
-
-        // your existing steps here
-        System.out.println("Running New User Journey Test...");
-=======
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
-
-public class NewUserJourney {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    @Test
-    public void logIn() {
         driver.get("https://www.mytheresa.com/en-de/");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        System.out.println("New User Journey Test Running...");
 
-        try {
-            WebElement cookieButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.cssSelector("button[class*='cookie']")));
-            cookieButton.click();
-        } catch (Exception ignored) { }
-
-        WebElement myAccount = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("a[href*='customer/account']")));
-
-        myAccount.click();
-
-        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
-        email.sendKeys("testuser@example.com");
-
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("TestPassword123");
-
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
-        loginButton.click();
->>>>>>> 8e3621820a5c3a5b6a2b14b2b2b82a1c33b7ff2d
+        // Add real test steps here
     }
 }
--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -1,33 +1,36 @@
-package testcases.mytheresa;
+package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class WebdriverSettings {
-<<<<<<< HEAD
-    public static WebDriver driver;
 
-    public static void driverSettings() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-        driver = new ChromeDriver();
-=======
+    protected WebDriver driver;
 
-    protected static WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
 
-    public static void initialize() {
+        WebDriverManager.chromedriver().setup();
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Important for Jenkins/Docker
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
     }
 
-    public static void tearDown() {
+    @AfterMethod
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
->>>>>>> 8e3621820a5c3a5b6a2b14b2b2b82a1c33b7ff2d
     }
 }
